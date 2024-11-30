adb7d876620819eecde354248960b91197f7efda
Jacky Wu <JackyWuCoder@gmail.com>
2024-11-29 18:43:02 -0800

    Start() and Update() Translations

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,7 +5,7 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        transform.Translate(0.01f, 0f, 0f);
     }
 
     // Update is called once per frame
